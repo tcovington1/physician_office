444839c34af15cdb5d6a3c44e229fd7fbd8a57b3
Taylor Covington <tcovingtondev@gmail.com>
2019-09-06 14:17:20 -0600

    appointment controller built, created show routes

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,30 @@
+class AppointmentsController < ApplicationController
+  before_action :set_physician
+  def index
+    @appointments = @appointment.physician.where(name: @physician.name)
+  end
+
+  def show
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  private
+  def physician_params
+    params.require(:billboard).permit(:name, :specialty)
+  end
+  
+  def appointment_params
+    params.require(:appointment).permit(:date, :time)
+  end
+
+  def set_physician
+    @physician = Physician.find(params[:physician_id])
+  end
+
+end
